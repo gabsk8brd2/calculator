ff44e64c965ca0df9401a68de7d82f7ae5943ac1
Gabriel Lima <aleluciano2017@gmail.com>
2023-10-10 08:23:57 -0400

    exercicio 1

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+/**
+ * Exercicio 1:
+ * 
+ * criar ua função que recebe uma lista de números e rotorna a média.
+ * 
+ * A função deve ser executada da seguinte forma:
+ * 
+ * const listofnumbers = [1,2,3,4,5]
+ * const average = calculateAverage(listofnumbers)
+ */
